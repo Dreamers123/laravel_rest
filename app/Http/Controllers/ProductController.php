4c7087bb44118a7bcd43bbb8996f729438531237
Abeer Azad <abeerazad19@gmail.com>
2020-01-08 13:08:26 +0600

    8th December 1st commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,9 +6,16 @@ use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Requests\ProductRequest;
+use Symfony\Component\HttpFoundation\Response;
 class ProductController extends Controller
 {
     
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+
     public function index()
     {
         return ProductCollection::collection(Product::paginate(20));
@@ -22,9 +29,19 @@ class ProductController extends Controller
     }
 
     
-    public function store(Request $request)
+   public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->user_id = auth()->id();
+        $product->save();
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     
